52f111a79045c3252fdd6e3e6f27fe810abb47ec
MizukiNakano <mizuki590@gmail.com>
2018-04-27 20:57:30 +0900

    add token class

---
+++ src/main/java/jp/mizunaka/mylang/Token.java
@@ -0,0 +1,37 @@
+package jp.mizunaka.mylang;
+
+public class Token {
+
+    private final String value;
+
+    public Token(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        if (!(o instanceof Token)) {
+            return false;
+        }
+        Token t = (Token) o;
+        if (this.value == null && t.value == null)  {
+            return true;
+        }
+        if (this.hashCode() != t.hashCode()) {
+            return false;
+        }
+        return this.value.equals(t.value);
+    }
+}
---
+++ src/test/java/jp/mizunaka/mylang/TokenTest.java
@@ -0,0 +1,27 @@
+package jp.mizunaka.mylang;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class TokenTest {
+    Token foo = new Token("foo");
+
+    @Test
+    public void getValue() {
+        assertEquals("foo", foo.getValue());
+    }
+
+    @Test
+    public void equal() {
+        assertTrue(foo.equals(new Token(foo.getValue())));
+    }
+
+    @Test
+    public void isNotequal() {
+        assertFalse(foo.equals(null));
+        assertFalse(foo.equals(new Token("hoge")));
+    }
+}
