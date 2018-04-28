388b4360f63a2d6c38d943bd5ae1ddd7e1c0ea93
MizukiNakano <mizuki590@gmail.com>
2018-04-28 15:54:26 +0900

    parse IdNode

---
+++ src/main/java/jp/mizunaka/mylang/ast/IdNode.java
@@ -0,0 +1,19 @@
+package jp.mizunaka.mylang.ast;
+
+import jp.mizunaka.mylang.Environment;
+
+public class IdNode extends ASTNode {
+    private final String name;
+
+    public IdNode(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Object eval(Environment env) throws MylangRuntimeException {
+        if (!env.has(name)) {
+            throw new MylangRuntimeException();
+        }
+        return env.get(name);
+    }
+}
