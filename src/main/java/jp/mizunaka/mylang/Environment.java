55b4325f955a5b4f318e9b5883724d7ac9e0e68e
MizukiNakano <mizuki590@gmail.com>
2018-04-28 21:06:38 +0900

    fix variable assign

--- src/main/java/jp/mizunaka/mylang/Environment.java
+++ src/main/java/jp/mizunaka/mylang/Environment.java
@@ -1,25 +1,54 @@
 package jp.mizunaka.mylang;
 
+import jp.mizunaka.mylang.ast.MylangRuntimeException;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Environment {
+    private Environment parent = null;
     private Map<String, Object> table = new HashMap<>();
 
     public Environment() {}
     public Environment(Environment env) {
-        this.table.putAll(env.table);
+        this.parent = env;
     }
 
     public boolean has(String name) {
-        return table.containsKey(name);
+        if (table.containsKey(name)) {
+            return true;
+        }
+        if (parent != null) {
+            return parent.has(name);
+        }
+        return false;
     }
 
     public Object get(String name) {
-        return table.get(name);
+        if (table.containsKey(name)) {
+            return table.get(name);
+        }
+        if (parent != null) {
+            return parent.get(name);
+        }
+        return null;
     }
 
-    public void put(String name, Object value) {
+    public void put(String name, Object value) throws MylangRuntimeException {
+        if (table.containsKey(name)) {
+            table.put(name, value);
+            return;
+        } else if (parent != null) {
+            parent.put(name, value);
+            return;
+        }
+        throw new MylangRuntimeException();
+    }
+
+    public void assign(String name, Object value) throws MylangRuntimeException {
+        if (table.containsKey(name)) {
+            throw new MylangRuntimeException();
+        }
         table.put(name, value);
     }
 }
